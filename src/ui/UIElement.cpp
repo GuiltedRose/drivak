a680cf0a78b4a597e75569f65c410915b926b42e
guiltedrose <guiltedrose@proton.me>
2025-04-01 05:37:00 -0400

    Fixed UIElement

--- src/ui/UIElement.cpp
+++ src/ui/UIElement.cpp
@@ -1,27 +1,29 @@
 #include "ui/UIElement.h"
 
-UIElement::UIElement() : position(0.0f), size(0.0f), visible(true) {}
+// This constructor was using `vec2(0.0f)` which doesn't exist
+// Update it to use your custom two-arg constructor
+UIElement::UIElement() : position(0.0f, 0.0f), size(0.0f, 0.0f), visible(true) {}
 
-void UIElement::setPosition(float x, float y) {
-    position = {x, y};
-}
-
-void UIElement::setSize(float width, float height) {
-    size = {width, height};
-}
-
-void UIElement::setVisible(bool show) {
-    visible = show;
-}
-
-glm::vec2 UIElement::getPosition() const {
+vec2 UIElement::getPosition() const {
     return position;
 }
 
-glm::vec2 UIElement::getSize() const {
+vec2 UIElement::getSize() const {
     return size;
 }
 
 bool UIElement::isVisible() const {
     return visible;
+}
+
+void UIElement::setPosition(float x, float y) {
+    position = vec2(x, y);
+}
+
+void UIElement::setSize(float width, float height) {
+    size = vec2(width, height);
+}
+
+void UIElement::setVisible(bool show) {
+    visible = show;
 }
