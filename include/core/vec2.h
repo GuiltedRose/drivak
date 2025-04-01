da86d6fb4a15ed33aed4a61a544fa96c5a580d47
guiltedrose <guiltedrose@proton.me>
2025-04-01 05:31:55 -0400

    Fixed vec2

--- include/core/vec2.h
+++ include/core/vec2.h
@@ -4,32 +4,32 @@ struct vec2 {
     float x = 0.0f;
     float y = 0.0f;
 
-    Vec2() = default;
-    Vec2(float x, float y) : x(x), y(y) {}
+    vec2() = default;
+    vec2(float x, float y) : x(x), y(y) {}
 
-    bool operator==(const Vec2& other) const {
+    bool operator==(const vec2& other) const {
         return x == other.x && y == other.y;
     }
 
-    bool operator!=(const Vec2& other) const {
+    bool operator!=(const vec2& other) const {
         return !(*this == other);
     }
 
-    Vec2 operator+(const Vec2& other) const {
-        return Vec2(x + other.x, y + other.y);
+    vec2 operator+(const vec2& other) const {
+        return vec2(x + other.x, y + other.y);
     }
 
-    Vec2 operator-(const Vec2& other) const {
-        return Vec2(x - other.x, y - other.y);
+    vec2 operator-(const vec2& other) const {
+        return vec2(x - other.x, y - other.y);
     }
 
-    Vec2& operator+=(const Vec2& other) {
+    vec2& operator+=(const vec2& other) {
         x += other.x;
         y += other.y;
         return *this;
     }
 
-    Vec2& operator-=(const Vec2& other) {
+    vec2& operator-=(const vec2& other) {
         x -= other.x;
         y -= other.y;
         return *this;
